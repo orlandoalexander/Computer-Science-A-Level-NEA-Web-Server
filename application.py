9ac4a4265962d42cc48b9ca4a5c42c15569e48cc
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-22 01:31:38 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,11 @@
+from flask import Flask, request
+from flask_restful import Api
+
+app = Flask(__name__) # the file is wrapped in the Flask constructer which enables the file to be a web-application
+api = Api(app)  # wrap 'app' variable in restful Api
+
+@app.route("/")
+def test():
+    return "Working"
+  
 
