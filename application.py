18d67bf3785b258449611f209f8a1bf5829be8af
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-22 22:51:01 +0100

    Update application.py

--- application.py
+++ application.py
@@ -21,8 +21,8 @@ def updateUsers():
         #query = "INSERT INTO users(accountID, firstName, surname, email, password) VALUES (request.form["accountID"], request.form["firstName"], request.form["surname"], request.form["email"], request.form["password"])"
         mycursor.execute(query)
         #mydb.commit()
-        test = (request.form["user"])
-        return type(test)
+        data = request.form.to_dict()
+        return data
     elif request.method == "GET":
         return "GET"
 
