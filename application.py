90db54dd576a01483f265b9456e17f8eb00c57b7
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-22 11:08:14 +0100

    Update application.py

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ from flask_restful import Api
 application = Flask(__name__) # the file is wrapped in the Flask constructer which enables the file to be a web-application
 api = Api(application)  # wrap 'app' variable in restful Api
 
+user = "orlandoalexander"
+passwd = "5martB3ll"
+
 @application.route("/")
 def test():
     return "Working"
