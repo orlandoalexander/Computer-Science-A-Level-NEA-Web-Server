6995f6dc9404da4027bfcb3dcc1cd49cf1f8df82
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-22 01:53:10 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
 from flask_restful import Api
 
-app = Flask(__name__) # the file is wrapped in the Flask constructer which enables the file to be a web-application
-api = Api(app)  # wrap 'app' variable in restful Api
+application = Flask(__name__) # the file is wrapped in the Flask constructer which enables the file to be a web-application
+api = Api(application)  # wrap 'app' variable in restful Api
 
 @app.route("/")
 def test():
     return "Working"
 
 if __name__ == "__main__":  # if the name of the file is the main program (not a module imported from another file)
-    app.run(debug=True)  # begins running the Api server
+    application.run(debug=True)  # begins running the Api server
 
 
