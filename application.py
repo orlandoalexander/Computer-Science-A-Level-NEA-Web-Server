a51ec9c5962abb68a1bc068f7819ccc4c9c436a0
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-22 13:08:19 +0100

    Update application.py

--- application.py
+++ application.py
@@ -2,14 +2,16 @@ from flask import Flask, request
 from flask_restful import Api
 
 application = Flask(__name__) # the file is wrapped in the Flask constructer which enables the file to be a web-application
-api = Api(application)  # wrap 'app' variable in restful Api
+api = Api(application)  # wrap 'application' variable in restful API
 
 
-@application.route("/")
+@application.route("/") 
+# homepage route 
 def test():
-    return "Working"
+    return "Working" # if the pipeline and server is working, the text 'Working' is displayed when the homepage is accessed 
 
 @application.route("/users")
+# route to modify the 'users' table
 def updateUsers():
     return "Hello"
     #if request.method == "POST":
