bbecddbd61b0db95ef7ba73cd7efebc656e70ff0
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-23 01:45:06 +0100

    Update application.py

--- application.py
+++ application.py
@@ -11,36 +11,54 @@ api = Api(application)  # wrap 'application' variable in restful API
 def test():
     return "Working" # if the pipeline and server is working, the text 'Working' is displayed when the homepage is accessed 
 
-@application.route("/users", methods = ["POST", "GET"])
+@application.route("/updateUsers", methods = ["POST"])
 # route to modify the 'users' table
 def updateUsers():
     data = request.form 
-    if data["method"] == "POST": # if the request from the mobile app is a 'POST' request (i.e. adding data to the 'users' table)
-        try:
-            mydb = mysql.connector.connect(host=(request.form["host"]), user=(request.form["user"]), passwd=(request.form["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-            mycursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
-            data = request.form # assigns the data sent to the API to a variable ('data')
-            query = "INSERT INTO users(accountID, firstName, surname, email, password) VALUES ('%s','%s','%s','%s','%s')" % (data['accountID'], data['firstName'], data['surname'], data['email'], data['password']) # MySQL query to add the data sent with the API to the appropriate columns in the 'users' table
-            mycursor.execute(query) # executes the query in the MySQL database
-            mydb.commit() # commits the changes to the MySQL database made by the executed query
-            return "success" # confirms that MySQL database was successfully updated
-        except:
-            return "error" # signifies that an error occured when adding the user's data in the 'users' table
+    try:
+        mydb = mysql.connector.connect(host=(request.form["host"]), user=(request.form["user"]), passwd=(request.form["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        mycursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        data = request.form # assigns the data sent to the API to a variable ('data')
+        query = "INSERT INTO users(accountID, firstName, surname, email, password) VALUES ('%s','%s','%s','%s','%s')" % (data['accountID'], data['firstName'], data['surname'], data['email'], data['password']) # MySQL query to add the data sent with the API to the appropriate columns in the 'users' table
+        mycursor.execute(query) # executes the query in the MySQL database
+        mydb.commit() # commits the changes to the MySQL database made by the executed query
+        return "success" # confirms that MySQL database was successfully updated
+    except:
+        return "error" # signifies that an error occured when adding the user's data in the 'users' table
         
-    elif data["method"] == "GET":
-        try:
-            mydb = mysql.connector.connect(host=(request.form["host"]), user=(request.form["user"]), passwd=(request.form["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-            mycursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
-            data = request.form # assigns the data sent to the API to a variable ('data')
-            query = "SELECT accountID FROM users WHERE email = '%s' AND password = '%s'" % (data['email'], data['password'])
-            mycursor.execute(query)
-            result = (mycursor.fetchone())[0]
-            if result == None:
-                return "none"
-            else:
-                return result
-        except:
-            return "error"
+@application.route("/verifyUser", methods = ["POST"])
+# route to verify a user's sign in details
+def verifyUser():
+    try:
+        mydb = mysql.connector.connect(host=(request.form["host"]), user=(request.form["user"]), passwd=(request.form["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        mycursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        data = request.form # assigns the data sent to the API to a variable ('data')
+        query = "SELECT accountID FROM users WHERE email = '%s' AND password = '%s'" % (data['email'], data['password'])
+        mycursor.execute(query)
+        result = (mycursor.fetchone())[0]
+        if result == None:
+            return "none"
+        else:
+            return result
+    except:
+        return "error"
+    
+@application.route("/verifyAccount", methods = ["POST"])
+# route to verify that a user's account doesn't already exist
+def verifyAccount():
+    try:
+        mydb = mysql.connector.connect(host=(request.form["host"]), user=(request.form["user"]), passwd=(request.form["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        mycursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        data = request.form # assigns the data sent to the API to a variable ('data')
+        query = "SELECT accountID FROM users WHERE email = '%s'" % (data['email'])
+        mycursor.execute(query)
+        result = (mycursor.fetchone())[0]
+        if result == None:
+            return "none"
+        else:
+            return "exists"
+    except:
+        return "error"
         
             
 
