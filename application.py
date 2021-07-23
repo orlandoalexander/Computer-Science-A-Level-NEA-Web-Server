b3456e25b96d0d5b465584efdab606c112d4c917
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-23 01:25:01 +0100

    Update application.py

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ def updateUsers():
         query = "SELECT accountID FROM users WHERE email = '%s' AND password = '%s'" % (data['email'], data['password'])
         mycursor.execute(query)
         result = mycursor.fetchone()
-        application.logging.info(result)
+        application.logger.info(result)
         if result == None:
             return "error"
         else:
