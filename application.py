c4ede87f7de0fd4f5dc9ff47258c26c52d138296
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-07-31 16:45:45 +0100

    Update application.py

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ def updateUsers():
     try:
         data = request.form # assigns the data sent to the API to a variable ('data')
         mydb = mysql.connector.connect(host=(data["host"]), user=(data["user"]), passwd=(data["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-        myCursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
         data = request.form # assigns the data sent to the API to a variable ('data')
         query = "INSERT INTO users(accountID, firstName, surname, email, password) VALUES ('%s','%s','%s','%s','%s')" % (data['accountID'], data['firstName'], data['surname'], data['email'], data['password']) # MySQL query to add the data sent with the API to the appropriate columns in the 'users' table
         myCursor.execute(query) # executes the query in the MySQL database
@@ -34,7 +34,7 @@ def verifyUser():
     try:
         data = request.form # assigns the data sent to the API to a variable ('data')
         mydb = mysql.connector.connect(host=(data["host"]), user=(data["user"]), passwd=(data["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-        myCursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
         data = request.form # assigns the data sent to the API to a variable ('data')
         query = "SELECT accountID FROM users WHERE email = '%s' AND password = '%s'" % (data['email'], data['password']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
         myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
@@ -49,7 +49,7 @@ def verifyAccount():
     try:
         data = request.form # assigns the data sent to the API to a variable ('data')
         mydb = mysql.connector.connect(host=(data["host"]), user=(data["user"]), passwd=(data["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-        myCursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        myCursor = mydb.cursor()  # initialises a cursor which allows communicationwith mydb (MySQL database)
         query = "SELECT accountID FROM users WHERE email = '%s'" % (data['email']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
         myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
         result = (myCursor.fetchone())[0] # returns the first result of the query result (accountID), if there is a result to be returned
@@ -63,7 +63,7 @@ def view_audioMessages():
     try:
         data = request.form # assigns the data sent to the API to a variable ('data')
         mydb = mysql.connector.connect(host=(data["host"]), user=(data["user"]), passwd=(data["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-        myCursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        myCursor = mydb.cursor()  # initialises a cursor which allows communicationwith mydb (MySQL database)
         query = "SELECT messageName, fileText FROM audioMessages WHERE accountID = '%s'" % (data['accountID']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
         myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
         result = myCursor.fetchall() # returns all the results of the query result (messageName and fileText), if there is a result to be returned
@@ -75,13 +75,27 @@ def view_audioMessages():
     except:
         return "error"
     
+@application.route("/verify_messageID", methods = ["POST"])
+# route to check whether the messageID that has been generated for an audio message does not already exist
+def verify_messageID():
+    try:
+        data = request.form # assigns the data sent to the API to a variable ('data')
+        mydb = mysql.connector.connect(host=(data["host"]), user=(data["user"]), passwd=(data["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
+        query = "SELECT messageID FROM audioMessages WHERE messageID = '%s'" % (data['messageID']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
+        myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
+        result = (myCursor.fetchone()[0]) # returns the first result of the query result (accountID), if there is a result to be returned
+        return "exists" # the string 'exists' is returned if the messageID generated is already used by another audio message in the 'audioMessages' table
+    except:
+        return "notExists" # the string 'notExists' is returned if the messageID generated is not already used by another audio message in the 'audioMessages' table
+    
     
 @application.route("/update_audioMessages", methods = ["POST"])
 # route to add data about a new audio message to the 'audioMessages' table
 def update_audioMessages():
     try:
         mydb = mysql.connector.connect(host=(request.form["host"]), user=(request.form["user"]), passwd=(request.form["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-        myCursor = mydb.cursor()  # initialises a cursor which allows you to communicate with mydb (MySQL database)
+        myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
         data = request.form # assigns the data sent to the API to a variable ('data')
         query = "INSERT INTO audioMessages (messageID, messageName, fileText, accountID) VALUES ('%s', '%s', '%s', '%s')" % (data['messageID'], data['messageName'], data['fileText'], data['accountID'])  # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
         myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
@@ -103,21 +117,6 @@ def uploadS3():
         return "success"
     except:
         return "error"
-    
-@application.route("/downloadS3", methods = ["POST"])
-# route to download byte data of the user's personalised audio messages
-def downloadS3(): 
-    #try:
-    data = request.form # assigns the metadata sent to the API to a variable ('data')
-    s3 = boto3.client("s3", aws_access_key_id=data["accessKey"], aws_secret_access_key=data["secretKey"]) # initialises a connection to the S3 client on AWS using the 'accessKey' and 'secretKey' sent to the API
-    s3.download_file(Filename="/tmp/audioMessage_download.txt", Bucket=data["bucketName"], Key=data["s3File"]) # downloads the txt file with the name equal to the concerned messageID from the S3 bucket called 'nea-audio-messages'. The name of the txt file when it is downloaded and stored temporarily on the AWS server
-    file = open("/tmp/audioMessage_download.txt", 'r') 
-        
-    return file
-    #return "success"
-    #except:
-        #return "error"
-              
         
         
 
