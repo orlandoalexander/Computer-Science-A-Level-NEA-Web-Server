795312567c61ffcc613620ea6f56bdf522b93437
orlandoalexander <alexandero09@berkhamsted.com>
2021-10-25 19:45:46 +0100

    updates

--- application.py
+++ application.py
@@ -163,7 +163,23 @@ def update_visitorLog():
         return "success"
     except:
         return "error"
-    
+
+@application.route("/update_knownFaces", methods = ["POST"])
+# route to add data about a new audio message to the 'audioMessages' table
+def update_knownFaces():
+    try:
+        with open("/etc/keys/db.json", "r") as file:
+            keys = json.load(file)
+        data = request.form # assigns the data sent to the API to a variable ('data')
+        mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
+                                       database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
+        query = "INSERT INTO knownFaces (faceID, faceName, accountID) VALUES ('%s', '%s', '%s')" % (data['faceID'], data['faceName'], data['accountID'])  # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
+        myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
+        mydb.commit() # commits the changes to the MySQL database made by the executed query
+        return "success"
+    except:
+        return "error"
     
 @application.route("/delete_audioMessages", methods = ["POST"])
 def delete_audioMessages():
@@ -253,7 +269,7 @@ def get_S3Key():
         accessKey_encoded = fernet.encrypt(keys_S3["accessKey"].encode()) # use Fernet class instance to encrypt the string - string must be encoded to byte string before it is encrypted
         secretKey_encoded = fernet.encrypt(keys_S3["secretKey"].encode()) # use Fernet class instance to encrypt the string - string must be encoded to byte string before it is encrypted
         encodedKeys_dict = {'accessKey_encoded': accessKey_encoded.decode(), 'secretKey_encoded': secretKey_encoded.decode()} # keys must be decoded to be jsonified and sent over API
-        return jsonify(encodedKeys_dict)
+        return encodedKeys_dict
     else:
         return "error"
 
