96f8a4ed882426252ce99ac21cd4c85fe1d096e8
orlandoalexander <alexandero09@berkhamsted.com>
2021-10-25 13:15:17 +0100

    updates

--- application.py
+++ application.py
@@ -14,29 +14,30 @@ api = Api(application)  # wrap 'application' variable in restful API
 def test():
     return "Hey there" # if the pipeline and server is working, the text 'Working' is displayed when the homepage is accessed
 
+
+
 @application.route("/updateUsers", methods = ["POST"])
 # route to add a new user to the 'users' table 
 def updateUsers():
     try:
         with open("/etc/keys/db.json", "r") as file:
-            passwds = json.load(file)
+            keys = json.load(file)
         data = request.form # assigns the data sent to the API to a variable ('data')
-        mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]), database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
         myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
         query = "INSERT INTO users(accountID, firstName, surname, email, password) VALUES ('%s','%s','%s','%s','%s')" % (data['accountID'], data['firstName'], data['surname'], data['email'], data['password']) # MySQL query to add the data sent with the API to the appropriate columns in the 'users' table
         myCursor.execute(query) # executes the query in the MySQL database
         mydb.commit() # commits the changes to the MySQL database made by the executed query
-        return passwds# confirms that MySQL database was successfully updated
+        return "success"# confirms that MySQL database was successfully updated
     except:
         return "error" # signifies that an error occured when adding the user's data in the 'users' table
         
 @application.route("/verifyUser", methods = ["POST"])
 # route to verify a user's sign in details
 def verifyUser():
-    with open("/etc/passwds/passwds.json", "r") as file:
-        passwds = json.load(file)
-    data = request.form # assigns the data sent to the API to a variable ('data')
-    mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+    with open("/etc/keys/db.json", "r") as file:
+        keys = json.load(file)
+    mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                    database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
     myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
     data = request.form # assigns the data sent to the API to a variable ('data')
@@ -52,10 +53,10 @@ def verifyUser():
 @application.route("/verifyAccount", methods = ["POST"])
 # route to verify that a user's account doesn't already exist
 def verifyAccount():
-    with open("/etc/passwds/passwds.json", "r") as file:
-        passwds = json.load(file)
+    with open("/etc/keys/db.json", "r") as file:
+        keys = json.load(file)
     data = request.form # assigns the data sent to the API to a variable ('data')
-    mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+    mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                    database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
     myCursor = mydb.cursor()  # initialises a cursor which allows communicationwith mydb (MySQL database)
     query = "SELECT EXISTS(SELECT * FROM users WHERE email = '%s')" % (data['email']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
@@ -70,10 +71,10 @@ def verifyAccount():
 # route to determine how many audio messages a particular user has and what the display names are and file details are for these messages
 def view_audioMessages():
     try:
-        with open("/etc/passwds/passwds.json", "r") as file:
-            passwds = json.load(file)
+        with open("/etc/keys/db.json", "r") as file:
+            keys = json.load(file)
         data = request.form # assigns the data sent to the API to a variable ('data')
-        mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+        mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                        database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
         myCursor = mydb.cursor()  # initialises a cursor which allows communicationwith mydb (MySQL database)
         query = "SELECT messageID, messageName, fileText FROM audioMessages WHERE accountID = '%s'" % (data['accountID']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
@@ -90,10 +91,10 @@ def view_audioMessages():
 @application.route("/verify_messageID", methods = ["POST"])
 # route to check whether the messageID that has been generated for an audio message does not already exist
 def verify_messageID():
-    with open("/etc/passwds/passwds.json", "r") as file:
-        passwds = json.load(file)
+    with open("/etc/keys/db.json", "r") as file:
+        keys = json.load(file)
     data = request.form # assigns the data sent to the API to a variable ('data')
-    mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+    mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                    database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
     myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
     query = "SELECT EXISTS(SELECT * FROM audioMessages WHERE messageID = '%s')" % (data['messageID']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
@@ -107,10 +108,10 @@ def verify_messageID():
 @application.route("/verify_messageName", methods = ["POST"])
 # route to check whether the message name that the user has inputted has already been assigned to one of their audio messages
 def verify_messageName():
-    with open("/etc/passwds/passwds.json", "r") as file:
-        passwds = json.load(file)
+    with open("/etc/keys/db.json", "r") as file:
+        keys = json.load(file)
     data = request.form # assigns the data sent to the API to a variable ('data')
-    mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+    mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                    database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
     myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
     query =  "SELECT EXISTS(SELECT * FROM audioMessages WHERE messageName = '%s' AND accountID = '%s')" % (data['messageName'], data['accountID']) # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
@@ -126,10 +127,10 @@ def verify_messageName():
 # route to add data about a new audio message to the 'audioMessages' table
 def update_audioMessages():
     try:
-        with open("/etc/passwds/passwds.json", "r") as file:
-            passwds = json.load(file)
+        with open("/etc/keys/db.json", "r") as file:
+            keys = json.load(file)
         data = request.form # assigns the data sent to the API to a variable ('data')
-        mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+        mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                        database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
         myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
         if data["initialCreation"] == "False":
@@ -146,10 +147,10 @@ def update_audioMessages():
 @application.route("/delete_audioMessages", methods = ["POST"])
 def delete_audioMessages():
     try:
-        with open("/etc/passwds/passwds.json", "r") as file:
-            passwds = json.load(file)
+        with open("/etc/keys/db.json", "r") as file:
+            keys = json.load(file)
         data = request.form # assigns the data sent to the API to a variable ('data')
-        mydb = mysql.connector.connect(host=(passwds["host"]), user=(passwds["user"]), passwd=(passwds["passwd"]),
+        mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
                                        database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
         myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
         query = "DELETE FROM audioMessages WHERE messageID = '%s'" % (data['messageID'])
@@ -164,11 +165,13 @@ def delete_audioMessages():
 # route to upload byte data of the user's personalised audio messages
 def uploadS3(): 
     try:
+        with open("/etc/keys/S3.json", "r") as file:
+            keys = json.load(file)
         data = request.form # assigns the metadata sent to the API to a variable ('data')
         file = request.files["file"] # assigns the txt file storing the bytes of the audio message to the variable 'file'
-        fileName = "/tmp/audioMessage_upload.pkl"
+        fileName = "/tmp/uploadFile.pkl"
         file.save(fileName) # temporarily saves the txt file in the "/tmp" folder on the AWS server
-        s3 = boto3.client("s3", aws_access_key_id=data["accessKey"], aws_secret_access_key=data["secretKey"]) # initialises a connection to the S3 client on AWS using the 'accessKey' and 'secretKey' sent to the API
+        s3 = boto3.client("s3", aws_access_key_id=keys["accessKey"], aws_secret_access_key=keys["secretKey"]) # initialises a connection to the S3 client on AWS using the 'accessKey' and 'secretKey' sent to the API
         s3.upload_file(Filename=fileName, Bucket=data["bucketName"], Key=data["s3File"]) # uploads the txt file to the S3 bucket called 'nea-audio-messages'. The name of the txt file when it is stored on S3 is the 'messageID' of the audio message which is being stored as a txt file.
         return "success"
     except:
@@ -178,9 +181,11 @@ def uploadS3():
 # route to upload byte data of the user's personalised audio messages
 def downloadTxt(): 
     try:
+        with open("/etc/keys/S3.json", "r") as file:
+            keys = json.load(file)
         data = request.form # assigns the metadata sent to the API to a variable ('data')
         fileName = "/tmp/audioMessage_download.pkl"
-        s3 = boto3.client("s3", aws_access_key_id=data["accessKey"], aws_secret_access_key=data["secretKey"]) # initialises a connection to the S3 client on AWS using the 'accessKey' and 'secretKey' sent to the API
+        s3 = boto3.client("s3", aws_access_key_id=keys["accessKey"], aws_secret_access_key=keys["secretKey"]) # initialises a connection to the S3 client on AWS using the 'accessKey' and 'secretKey' sent to the API
         s3.download_file(Filename=fileName, Bucket=data["bucketName"], Key=data["s3File"])  # downloads the txt file with the name equal to the concerned messageID from the S3 bucket called 'nea-audio-messages'. The name of the txt file when it is downloaded and stored temporarily on the AWS server
         return send_file(fileName, as_attachment = True)
     except:
