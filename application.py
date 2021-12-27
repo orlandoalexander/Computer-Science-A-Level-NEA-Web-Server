35927f070f0966aadb93d79cda3533ae005ddb3c
orlandoalexander <alexandero09@berkhamsted.com>
2021-12-27 15:19:29 +0000

    updates

--- application.py
+++ application.py
@@ -259,18 +259,22 @@ def get_S3Key():
 @application.route("/update_SmartBellIDs", methods = ["POST"])
 # route to add data about a new audio message to the 'audioMessages' table
 def update_SmartBellIDs():
-    with open("/etc/keys/db.json", "r") as file:
-        keys = json.load(file)
-    data = request.form # assigns the data sent to the API to a variable ('data')
-    mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
-                                   database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
-    myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
-    if 'accountID' not in data: # if request is to add a new id, not a new accountID
-        query = "INSERT INTO SmartBellIDs (id) VALUES ('%s')" % (data['id'])  # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
-    else:
-        query = "UPDATE SmartBellIDs SET accountID = ('%s') WHERE id = '%s'" % (data['accountID'], data['id'])  # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
-    myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
-    mydb.commit() # commits the changes to the MySQL database made by the executed query
+    try:
+        with open("/etc/keys/db.json", "r") as file:
+            keys = json.load(file)
+        data = request.form # assigns the data sent to the API to a variable ('data')
+        mydb = mysql.connector.connect(host=(keys["host"]), user=(keys["user"]), passwd=(keys["passwd"]),
+                                       database="ebdb")  # initialises the database using the details sent to API, which can be accessed with the 'request.form()' method
+        myCursor = mydb.cursor()  # initialises a cursor which allows communication with mydb (MySQL database)
+        if 'accountID' not in data: # if request is to add a new id, not a new accountID
+            query = "INSERT INTO SmartBellIDs (id) VALUES ('%s')" % (data['id'])  # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
+        else:
+            query = "UPDATE SmartBellIDs SET accountID = ('%s') WHERE id = '%s'" % (data['accountID'], data['id'])  # 'query' variable stores string with MySQL command that is to be executed. The '%s' operator is used to insert variable values into the string.
+        myCursor.execute(query) # the query is executed in the MySQL database which the variable 'myCursor' is connected to
+        mydb.commit() # commits the changes to the MySQL database made by the executed query
+        return 'success'
+    except:
+        return 'error'
 
 
 @application.route("/verify_SmartBellID", methods=["POST"])
